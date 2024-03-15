8da9424234eeccef7448020dcd1868c2ec245646
Inn21 <innicasandretorz@gmail.com>
2024-03-15 10:37:23 +0200

    Module 1 var 2

---
+++ Module1/CalendarEvent.cs
@@ -0,0 +1,29 @@
+﻿namespace Module1
+{
+    public enum DaysOfWeek
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    }
+    internal class CalendarEvent
+    {
+        public string EventName { get; private set; }
+        public DaysOfWeek EventDay { get; private set; }
+
+        public CalendarEvent(string eventName = "N/A", DaysOfWeek eventDay = DaysOfWeek.Sunday)
+        {
+            EventName = eventName;
+            EventDay = eventDay;
+        }
+
+        public void PrintEventDetails()
+        {
+            Console.WriteLine($"Подія {EventName} відбудеться в день {EventDay}");
+        }
+    }
+}
---
+++ Module1/Program.cs
@@ -0,0 +1,19 @@
+﻿//Створіть перелік(enum) DaysOfWeek, який містить назви днів тижня.
+
+//Далі створіть клас CalendarEvent, який представляє подію у календарі і має наступні властивості:
+//      EventName(назва події)
+//      EventDay(день події з переліку DaysOfWeek)
+//      Створіть конструктор для класу CalendarEvent, що дозволяє встановити назву події та день.
+//      Напишіть метод PrintEventDetails(), який виводить інформацію про подію на консоль у форматі: "Подія EventName відбудеться в день EventDay".
+
+//Створіть об'єкт класу CalendarEvent, встановіть йому значення та виведіть інформацію про подію на консоль.
+
+using Module1;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        CalendarEvent ev = new CalendarEvent("Module", DaysOfWeek.Friday);
+        ev.PrintEventDetails();
+    }
+}
